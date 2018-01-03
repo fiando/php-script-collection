b7d643b293bb475a8a4fd8a68f12ce67044d728b
Bobby Fiando <bobfiando@gmail.com>
2018-01-03 18:33:42 +0700

    Strx
    String Manipulation Function

---
+++ strx/tutorial.php
@@ -0,0 +1,24 @@
+<?php require("strx.class.php"); ?>
+<?php web_header(); ?>
+        <div class="box">
+            <div class="text tutorial">
+                <h3>Panduan Singkat</h3><hr />
+                <span>
+                    <h4>Langkah-langkah : </h4>
+                    <ol>
+                        <li>Masukan Teks</li>
+                        <li>Pilih Opsi untuk mengubah teks yang diinginkan</li>
+                        <li>Klik/Tekan <b>Manipulasi</b> untuk memproses</li>
+                    </ol>
+                    <h4>Fungsi yang tersedia :</h4>
+                    <ul>
+                        <li>Umum
+                        </li>
+                        <li>Enkripsi</li>
+                        <li>Encode</li>
+                        <li>Enkripsi Sederhana</li>
+                        <li>Format Teks</li>
+                    </ul>
+                </span>
+            </div>
+<?php web_footer(); ?>
